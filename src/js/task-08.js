9d9102b744ce6e9c23e208e05aa1e33c816f0abe
eugenebatanov <evgeni.batanov@gmail.com>
2023-03-22 00:49:36 +0100

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onInputEvent);
+
+function onInputEvent(event) {
+  event.preventDefault();
+
+  const formInputs = event.currentTarget.elements;
+  const email = formInputs.email.value;
+  const password = formInputs.password.value;
+
+  if (email === "" || password === "") {
+    alert("Всі поля повинні бути заповнені");
+  }
+
+  const formData = new FormData(event.currentTarget);
+  formData.forEach((value, name) => {
+    console.log("Name:", name, ";", "Value:", value);
+  });
+
+  form.reset();
+}

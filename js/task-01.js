e9abe99e8318ddc36726f4089e06bf0ab586520d
eugenebatanov <evgeni.batanov@gmail.com>
2023-03-23 00:35:12 +0100

    add prettier

--- js/task-01.js
+++ js/task-01.js
@@ -1,8 +1,8 @@
-const liItem = document.querySelectorAll(".item");
+const liItem = document.querySelectorAll('.item');
 
-console.log("Number of categories: ", liItem.length);
+console.log('Number of categories: ', liItem.length);
 
-liItem.forEach((el) => {
-  console.log("Category: ", el.firstElementChild.textContent);
-  console.log("Elements: ", el.lastElementChild.children.length);
+liItem.forEach(el => {
+  console.log('Category: ', el.firstElementChild.textContent);
+  console.log('Elements: ', el.lastElementChild.children.length);
 });
